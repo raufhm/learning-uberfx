7c1471d67ec3d53cb58aa94a7ec081df9334b5e7
Abdul Rauf <abdulraufmustakim@gmail.com>
2023-06-12 13:46:19 +0700

    initial

--- uberfx/common.go
+++ uberfx/common.go
@@ -2,19 +2,13 @@ package uberfx
 
 import "go.uber.org/fx"
 
-func invoker() fx.Option {
-	return fx.Options(
-		InvokeRegisterRoutes(),
-		InvokeHTTPServer(),
-	)
-}
-
 func LoadOptions() []fx.Option {
 	return []fx.Option{
 		provideDependencies(),
 		provideHandler(),
 		provideService(),
 		provideRepository(),
-		invoker(),
+		InvokeRegisterRoutes(),
+		InvokeHTTPServer(),
 	}
 }
